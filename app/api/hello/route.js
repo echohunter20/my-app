a88d71f95ebf37dd98ce1a120b313715cf517505
hunter2023 <hunteryang2023@163.com>
2023-08-01 14:48:24 +0800

    update images

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -6,7 +6,7 @@ export async function GET(request) {
 	const cookieStore = cookies();
 	const token = cookieStore.get('token');
 
-	return new Response('Hello, Next.js!', {
+	return new Response('Hello, Next.js!12312312', {
 		status: 200,
 		headers: { 'Set-Cookie': `token=${token}` },
 	});
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+//https://imagedelivery.net/FCXqi8BC3uKlNw5AGYVCpw/b1b13cec-dfa0-4464-7052-8aa9e5296400/public
 
 const Home = () => {
   return (
@@ -8,6 +9,10 @@ const Home = () => {
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
+      <img
+            src="https://imagedelivery.net/FCXqi8BC3uKlNw5AGYVCpw/b1b13cec-dfa0-4464-7052-8aa9e5296400/public"
+            className=" h-[15px] w-auto xl:h-[20px] 2xl:h-[25px] 3xl:h-[30px] "
+          />
     </div>
   )
 }
